f3bc0361bc6906be634acbabc229d4fe099182ad
bushblade <bushblade@gmail.com>
2018-01-29 21:48:07 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const output = document.getElementById("output");
+const container = document.querySelector(".container");
+const calculate = document.getElementById("calculate");
+const clear = document.getElementById("clear");
+const operators = ["/", "*", "-", "+", "."];
+
+container.addEventListener("click", buttonClick);
+calculate.addEventListener("click", calc);
+clear.addEventListener("click", reset);
+
+function buttonClick(e) {
+  let btnText = e.target.textContent;
+  let lastChar = output.textContent[output.textContent.length - 1];
+  if (e.target.classList.contains("is-primary")) {
+    output.textContent += btnText;
+  } else if (e.target.classList.contains("is-info")) {
+    !operators.includes(lastChar) ? (output.textContent += btnText) : false;
+  }
+}
+
+function reset() {
+  output.innerText = "";
+}
+
+function calc() {
+  output.textContent = eval(output.textContent);
+}
