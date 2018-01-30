c852d250169477b59d3dffb4103d6203faa55603
bushblade <bushblade@gmail.com>
2018-01-30 13:18:39 +0000

    refactored

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const keyArray = [111, 106, 109, 103, 104, 105, 107, 100, 101, 102, 97, 98, 99, 
 container.addEventListener("click", buttonClick);
 calculate.addEventListener("click", calc);
 clear.addEventListener("click", reset);
+document.addEventListener("keydown", keyPress);
 
 function buttonClick(e) {
   let btnText = e.target.textContent;
@@ -19,6 +20,20 @@ function buttonClick(e) {
   }
 }
 
+function keyPress(e) {
+  let keyText = e.key;
+  let lastChar = output.textContent[output.textContent.length - 1];
+  if (keyArray.includes(e.keyCode)) {
+    if (!operators.includes(keyText)) {
+      output.textContent += keyText;
+    } else if (!operators.includes(lastChar) && output.textContent !== '') {
+      output.textContent += keyText;
+    }
+  } else if (e.keyCode === 13) {
+    calc();
+  }
+}
+
 function reset() {
   output.innerText = "";
 }
@@ -26,17 +41,3 @@ function reset() {
 function calc() {
   output.textContent = eval(output.textContent);
 }
-
-document.addEventListener("keydown", event => {
-  let keyText = event.key;
-  let lastChar = output.textContent[output.textContent.length - 1];
-  if (keyArray.includes(event.keyCode)) {
-    if (!operators.includes(event.key)) {
-      output.textContent += keyText;
-    } else if (!operators.includes(lastChar) && output.textContent !== '') {
-      output.textContent += keyText;
-    }
-  } else if (event.keyCode === 13) {
-    calc();
-  }
-});
