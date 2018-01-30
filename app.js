ab4f787fbde349ceb1810d2e1ecb0bf42841e9db
bushblade <bushblade@gmail.com>
2018-01-30 12:57:58 +0000

    added numpad use

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const output = document.getElementById("output");
 const container = document.getElementById("calc-wrapper");
 const calculate = document.getElementById("calculate");
 const clear = document.getElementById("clear");
-const operators = ["/", "*", "-", "+", "."];
+const operators = Array.from(document.querySelectorAll('.is-info')).map(x => x.textContent);
+const keyArray = [111, 106, 109, 103, 104, 105, 107, 100, 101, 102, 97, 98, 99, 96, 110];
 
 container.addEventListener("click", buttonClick);
 calculate.addEventListener("click", calc);
@@ -14,7 +15,7 @@ function buttonClick(e) {
   if (e.target.classList.contains("is-primary")) {
     output.textContent += btnText;
   } else if (e.target.classList.contains("is-info")) {
-    !operators.includes(lastChar) ? (output.textContent += btnText) : false;
+    !operators.includes(lastChar) && output.textContent !== '' ? output.textContent += btnText : false;
   }
 }
 
@@ -25,3 +26,17 @@ function reset() {
 function calc() {
   output.textContent = eval(output.textContent);
 }
+
+document.addEventListener("keydown", event => {
+  let keyText = event.key;
+  let lastChar = output.textContent[output.textContent.length - 1];
+  if (keyArray.includes(event.keyCode)) {
+    if (!operators.includes(event.key)) {
+      output.textContent += keyText;
+    } else if (!operators.includes(lastChar) && output.textContent !== '') {
+      output.textContent += keyText;
+    }
+  } else if (event.keyCode === 13) {
+    calc();
+  }
+});
